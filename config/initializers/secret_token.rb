d68344442fd2fa976e0946157bf1ab547695851f
Joel Mahoney <joelmahoney@gmail.com>
2013-02-16 11:56:58 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streetcred::Application.config.secret_token = '619fc5f9123fb2d522912650d9070a13fa9e1dbc2cf688ad79633d717b94f58804750b77959b690f8b9210d4feef5e387cfcb43296e68844bcc0cbea872036e7'
