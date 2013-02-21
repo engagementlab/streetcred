19d06910aaac8feb7f14f886d942f88f7dd5d85c
Joel Mahoney <joelmahoney@gmail.com>
2013-02-21 07:59:34 -0800

    Make campaign awards links pretty

--- app/helpers/admin/campaigns_helper.rb
+++ app/helpers/admin/campaigns_helper.rb
@@ -1,2 +1,12 @@
 module Admin::CampaignsHelper
+  
+  def awards_count_helper(campaign)
+    if campaign.awards.blank? || campaign.awards.count == 0
+      link_to 'Add Awards', new_admin_campaign_award_path(campaign)
+    elsif campaign.awards.count == 1
+      link_to "#{campaign.awards.count} Award", admin_campaign_awards_path(campaign)
+    else
+      link_to "#{campaign.awards.count} Awards", admin_campaign_awards_path(campaign)
+    end
+  end
 end
