e96aa084507b0c287e75012aa061cbe51ee8c17d
Joel Mahoney <joelmahoney@gmail.com>
2013-02-21 08:17:03 -0800

    Add helper for campaign awards count link

--- app/helpers/admin/campaigns_helper.rb
+++ app/helpers/admin/campaigns_helper.rb
@@ -2,7 +2,7 @@ module Admin::CampaignsHelper
   
   def awards_count_helper(campaign)
     if campaign.awards.blank? || campaign.awards.count == 0
-      link_to 'Add Awards', new_admin_campaign_award_path(campaign)
+      link_to '0 Awards', new_admin_campaign_award_path(campaign)
     elsif campaign.awards.count == 1
       link_to "#{campaign.awards.count} Award", admin_campaign_awards_path(campaign)
     else
