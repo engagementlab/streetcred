ab45f97d127a96622dfac4b33703da3c72432220
Joel Mahoney <joelmahoney@gmail.com>
2013-02-23 19:06:56 -0800

    Fix missing fields_for params id by using :index instead of :child_index

--- app/helpers/admin/awards_helper.rb
+++ app/helpers/admin/awards_helper.rb
@@ -2,7 +2,7 @@ module Admin::AwardsHelper
   def link_to_add_fields(name, parent, f, association)
     new_object = parent.send(association).new
     id = new_object.object_id
-    fields = f.fields_for("required_actions_attributes[]", new_object) do |builder|
+    fields = f.fields_for("required_actions_attributes[]", new_object, index: id) do |builder|
       render(association.to_s.singularize + "_fields", f: builder)
     end
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
