6c66c5a165db734a018f55a299f56f859601cc7e
Joel Mahoney <joelmahoney@gmail.com>
2013-03-07 20:55:38 -0800

    Add description, start_time and end_time to campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -5,6 +5,9 @@ class Campaign
   has_and_belongs_to_many :awards
   
   field :name, type: String
+  field :description, type: String
+  field :start_time, type: DateTime
+  field :end_time, type: DateTime
   
   validates_presence_of :name
   
