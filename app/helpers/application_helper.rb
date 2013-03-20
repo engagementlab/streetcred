7a12c5578782ce56c43bbb3b134c2bbd393859f7
Joel Mahoney <joelmahoney@gmail.com>
2013-03-20 10:30:59 -0700

    Fix progress_toward_award helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def progress_toward_award_helper(user, award)
-    "#{self.matching_actions(award).count} out of #{award.required_occurrences}"
+    "#{user.matching_actions(award).count} out of #{award.required_occurrences}"
   end
 end
