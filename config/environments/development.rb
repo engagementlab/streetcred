25d3c36b2c8c7a5afdb92bd4efe645efe028696a
Joel Mahoney <joelmahoney@gmail.com>
2013-03-20 09:59:06 -0700

    Update ActionMailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,15 @@ Streetcred::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
