d185b5278f1fe2d05c3611ffa954a22932b1b1b0
Joel Mahoney <joelmahoney@gmail.com>
2013-03-19 17:23:43 -0700

    Add SendGrid config

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,12 @@
+class NotificationMailer < ActionMailer::Base
+  default from: "notifications@streetcred.us"
+  
+  def status_email(user, action)
+    @user = user
+    @action = action
+    @earned_awards = user.awards_earned_by_action(action)
+    @in_progress_awards = user.awards_in_progress_by_action(action)
+    mail(:to => user.email,
+         :subject => "Nice Work!")
+  end
+end
