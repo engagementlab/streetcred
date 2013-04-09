8d50604b6329dc360497456043098df7615aa879
Joel Mahoney <joelmahoney@gmail.com>
2013-04-09 07:06:32 -0700

    Filter foursquare checkins

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module Streetcred
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :checkin]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
