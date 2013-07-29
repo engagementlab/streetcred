aedfc2083e3c1f9440fccbf768e40f3bcb796e12
Joel Mahoney <joelmahoney@gmail.com>
2013-07-29 11:53:42 -0700

    Add campaigns controller

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,22 @@
+class CampaignsController < ApplicationController
+  def index
+    @active_campaigns = Campaign.active
+    @completed_campaigns = Campaign.completed
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @active_campaigns = Campaign.active
+    @completed_campaigns = Campaign.completed
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
+  end
+end
