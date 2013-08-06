4336bb2c0beddb7e366cf55888458012b26af530
Joel Mahoney <joelmahoney@gmail.com>
2013-08-06 13:09:16 -0700

    Show badge placeholder image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,6 +25,8 @@ module ApplicationHelper
 				image_tag campaign.community_badge.url(:badge)
 			elsif campaign.progress_by_individual(user) >= 1.0
 				image_tag campaign.individual_badge.url(:badge)
+			else
+				image_tag '/assets/badge-blank.png'
 			end
 		else
 			image_tag campaign.badge_icon.url(:badge)
