3535581c590836b8f25a01bac8959cfad54ed76d
Joel Mahoney <joelmahoney@gmail.com>
2013-08-12 07:58:57 -0700

    Work on map styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,18 @@ class UsersController < ApplicationController
     @active_campaigns = Campaign.active
     @completed_campaigns = Campaign.completed
     @earned_campaigns = @completed_campaigns.select {|x| x.requirements_met_by_individual?(@user)}
-    @json = @user.actions.to_gmaps4rails
+
+    @json = @user.actions.to_gmaps4rails do |action, marker|
+      # marker.infowindow render_to_string(:partial => "/users/my_template", :locals => { :object => user})
+      marker.picture({
+                      :picture => "/assets/marker.png",
+                      :width   => 28,
+                      :height  => 25
+                     })
+      # marker.title   "#{action.action_type.try(:name)}<br />#{action.created_at}"
+      # marker.sidebar "i'm the sidebar"
+      marker.json({ :id => action.id })
+    end
 
     respond_to do |format|
       format.html # show.html.erb
