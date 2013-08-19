8760de7945006486cb594feef2dc4f0358dfafe3
Joel Mahoney <joelmahoney@gmail.com>
2013-08-19 07:55:47 -0700

    Work on home page styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Streetcred::Application.routes.draw do
   root :to => 'pages#index' 
   
   resources :campaigns, :only => [:index, :show]
+  resources :pages, :only => [:index, :show]
   resources :users, :only => [:index, :show]
   
   devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks", :sessions => 'sessions', :registrations => 'registrations'}
