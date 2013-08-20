258b978426999b60a63fb83294b66df54c58e6dd
Joel Mahoney <joelmahoney@gmail.com>
2013-08-20 00:32:42 -0700

    Work on home page styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
 
 	def index
-		
+		@current_campaign = Campaign.active.last
+
+		@json = Action.all.to_gmaps4rails do |action, marker|
+			# marker.infowindow render_to_string(:partial => "/users/my_template", :locals => { :object => user})
+			marker.picture({
+										:picture => "/assets/marker.png",
+										:width   => 28,
+										:height  => 25
+									 })
+			# marker.title   "#{action.action_type.try(:name)}<br />#{action.created_at}"
+			# marker.sidebar "i'm the sidebar"
+			marker.json({ :id => action.id })
+		end
 	end
 end
