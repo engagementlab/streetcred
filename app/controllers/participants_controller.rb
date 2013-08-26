6f38ed41acd3d026fc10e468779a79171023fe27
Joel Mahoney <joelmahoney@gmail.com>
2013-08-26 14:09:39 -0700

    Fix maps markers

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -14,7 +14,7 @@ class ParticipantsController < ApplicationController
     @active_campaigns = Campaign.active
     @completed_campaigns = Campaign.completed
     @earned_awards = @completed_campaigns.select {|x| x.requirements_met_by_individual?(@participant)}
-    gon.markers = @participant.actions.all.reject{|x| x.latitude.blank? || x.longitude.blank?}.collect {|x| {type: 'Feature', geometry: {type: 'Point', coordinates: [x.longitude, x.latitude]}, properties: { title: x.action_type.try(:channel).try(:name), description: "#{x.action_type.try(:name)}<br />#{x.created_at.strftime('%m/%d/%Y')}", 'marker-size' => 'small', 'marker-color' => '#ff502d'}}}
+    gon.markers = @participant.actions.all.reject{|x| x.latitude.blank? || x.longitude.blank?}.collect {|x| {type: 'Feature', geometry: {type: 'Point', coordinates: [x.longitude, x.latitude]}, properties: { title: x.user.try(:display_name), description: "#{x.action_type.try(:channel).try(:name)}<br />#{x.action_type.try(:name)}<br />#{x.created_at.strftime('%m/%d/%Y')}", 'marker-size' => 'small', 'marker-color' => '#ff502d'}}}
 
 
     respond_to do |format|
