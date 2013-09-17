f455cf65ee26b642b50778da5a5b8ff908e48185
Joel Mahoney <joelmahoney@gmail.com>
2013-09-17 06:04:09 -0700

    Formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-	def after_sign_in_path_for(resource)
-	 participant_path(resource)
-	end
+  def after_sign_in_path_for(resource)
+  	participant_path(resource)
+  end
+
 end
