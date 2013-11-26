87659fb17afc57bbd1d65bf644c10d7c452fefe3
Joel Mahoney <joelmahoney@gmail.com>
2013-11-26 12:54:27 -0800

    Add OpenBadges logo under badge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,10 +23,16 @@ module ApplicationHelper
   	if user.present?
       # only show the community badge if the user has earned the individual badge
 			if campaign.progress_by_individual(user) >= 1 && campaign.progress_by_community >= 1
-				link_to image_tag(campaign.community_badge.url(:badge)), '#', class: 'openbadge-link', :data => { :api => "#{badge_api_path(user, campaign)}" }
+				"#{link_to image_tag(campaign.community_badge.url(:badge)), '#', class: 'openbadge-link', :data => { :api => '#{badge_api_path(user, campaign)}' }}" +
+        "<div style='margin-top: 15px;'>" +
+        "#{image_tag('send_to_open_badges.png', class: 'openbadge-link')}" +
+        "</div>"
 			elsif campaign.progress_by_individual(user) >= 1
-				link_to image_tag(campaign.individual_badge.url(:badge)), '#', class: 'openbadge-link', :data => { :api => "#{badge_api_path(user, campaign)}" }
-			else
+				"#{link_to image_tag(campaign.individual_badge.url(:badge)), '#', class: 'openbadge-link', :data => { :api => '#{badge_api_path(user, campaign)}' }}" +
+        "<div style='margin-top: 15px;'>" +
+        "#{image_tag('send_to_open_badges.png', class: 'openbadge-link')}" +
+        "</div>"			
+      else
 				image_tag '/assets/badge-blank.png'
 			end
 		else
