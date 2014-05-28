83c28e25dcea4cb024346fde8cbd0f9846ea84b5
Leonardo Souza <leonardocsouza@gmail.com>
2014-05-28 01:32:43 -0400

    Add backend integration to new participant dashboard design

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -15,7 +15,8 @@ class ParticipantsController < ApplicationController
     @expired_contributed_to_campaigns = @participant.expired_contributed_to_campaigns
     @earned_awards = @participant.completed_campaigns.select {|x| x.requirements_met_by_individual?(@participant)}
     @actions_with_text = @participant.actions.or({:body.exists => true}, {:subject.exists => true}).desc(:created_at).limit(5)
-    @actions_with_photos = @participant.actions.or({:image_url.exists => true}, {:photo_url.exists => true}).desc(:created_at).limit(5)
+    @actions_with_photos = @participant.actions.or({:image_url.exists => true}, {:photo_url.exists => true}).desc(:created_at).limit(16)
+    @actions_recent = @participant.actions.desc(:created_at).limit(5)
     gon.markers = @participant.actions.all.reject {|x| x.latitude.blank? || x.longitude.blank?}.collect {|x| {type: 'Feature', geometry: {type: 'Point', coordinates: [x.longitude, x.latitude]}, properties: { title: x.user.try(:display_name), description: "#{x.action_type.try(:channel).try(:name)}<br />#{x.action_type.try(:name)}<br />#{x.created_at.strftime('%m/%d/%Y')}", 'marker-size' => 'small', 'marker-color' => '#ff502d'}}}
 
 
