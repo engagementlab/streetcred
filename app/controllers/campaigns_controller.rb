5fb251b4da171b5bce92bb1174f3c26af5fed227
Leonardo Souza <leonardocsouza@gmail.com>
2014-06-11 01:59:45 -0400

    Add backend integration for new design of campaign page

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -11,7 +11,9 @@ class CampaignsController < ApplicationController
 
   def show
     @campaign = Campaign.find(params[:id])
-    @badges_earned = @campaign.users
+    #@badges_earned = @campaign.users
+    @actions_with_photos = @campaign.actions.or({:image_url.exists => true}, {:photo_url.exists => true}).desc(:created_at).limit(16)
+    gon.markers = @campaign.actions.all.reject {|x| x.latitude.blank? || x.longitude.blank?}.collect {|x| {type: 'Feature', geometry: {type: 'Point', coordinates: [x.longitude, x.latitude]}, properties: { title: x.user.try(:display_name), description: "#{x.action_type.try(:channel).try(:name)}<br />#{x.action_type.try(:name)}<br />#{ActionController::Base.helpers.link_to(x.created_at.strftime('%m/%d/%Y'), campaign_action_path(x.user, x))}", 'marker-size' => 'small', 'marker-color' => '#ff502d'}}}
 
     respond_with @campaign
   end
