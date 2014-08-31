046c72142b9e34da3885903b852841369571bdb3
Leonardo Souza <leonardocsouza@gmail.com>
2014-08-31 09:50:10 -0400

    Fix use of helpers from controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
 
 	def index
 		@current_campaign = Campaign.current
-		gon.markers = Action.all.reject {|x| x.latitude.blank? || x.longitude.blank? || !x.user.try(:claimed?) || !x.user.try(:shared?) || !x.user.try(:map_visible?)}.collect {|x| {type: 'Feature', geometry: {type: 'Point', coordinates: [x.longitude, x.latitude]}, properties: { title: x.user.try(:display_name), description: "#{x.action_type.try(:channel).try(:name)}<br />#{x.action_type.try(:name)}<br />#{formatted_date(x.created_at)}", 'marker-size' => 'small', 'marker-color' => '#ff502d'}}}
+		gon.markers = Action.all.reject {|x| x.latitude.blank? || x.longitude.blank? || !x.user.try(:claimed?) || !x.user.try(:shared?) || !x.user.try(:map_visible?)}.collect {|x| {type: 'Feature', geometry: {type: 'Point', coordinates: [x.longitude, x.latitude]}, properties: { title: x.user.try(:display_name), description: "#{x.action_type.try(:channel).try(:name)}<br />#{x.action_type.try(:name)}<br />#{view_context.formatted_date(x.created_at)}", 'marker-size' => 'small', 'marker-color' => '#ff502d'}}}
 	end
 end
